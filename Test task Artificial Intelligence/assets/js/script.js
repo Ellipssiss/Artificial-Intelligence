3c46ca96fc37993a2539fb48572eafbf65b7374e
Ellipssiss <118723378+Ellipssiss@users.noreply.github.com>
2023-04-16 14:24:31 +0300

    hovers&plugins manual push

--- Test task Artificial Intelligence/assets/js/script.js
+++ Test task Artificial Intelligence/assets/js/script.js
@@ -1,3 +1,4 @@
+import Splide from '@splidejs/splide';
 $(document).ready(function () {
 
 
@@ -8,10 +9,14 @@ $(document).ready(function () {
 		}, 50);
 	})
 
-	$('.burgerBtn').click(function () {
-		$('.backgroundBurgerMenu').addClass('show');
-		$('.burgerMenuContainer').addClass('show');
+	// $('.burgerBtn').click(function () {
+	// 	$('.backgroundBurgerMenu').addClass('show');
+	// 	$('.burgerMenuContainer').addClass('show');
 
+	// })
+
+	$('.burgerBtn').click(function () {
+		$('.burgerMenu').addClass('show');
 	})
 
 	$('.exitBtnBurgerMenu').click(function () {
@@ -25,10 +30,89 @@ $(document).ready(function () {
 		$('.burgerMenu').removeClass('show');
 	});
 
+	$('.backgroundBurgerMenu').click(function () {
+		$('.burgerMenu').removeClass('show');
+	});
 
-	$('.burgerBtn').click(function () {
-		$('.burgerMenu').addClass('show');
+
+
+	$('.selectboxit-container').click(function () {
+		$('.languageSwitch__mark').toggleClass('up');
+	});
+
+
+
+	// ------------------------------------------------------
+	const creators = $('.creators');
+	creators.click(function () {
+		$(this).toggleClass('show');
+		$(".creators .item_content").stop().slideUp();
+		$(".creators.show .item_content").stop().slideDown();
 	})
+
+
+	$('.artists').click(function () {
+		$('.artists').toggleClass('show');
+	})
+
+	$('.musicians').click(function () {
+		$('.musicians').toggleClass('show');
+	})
+	// ------------------------------------------------------
+	const footer__column = $('.footer__column');
+	footer__column.click(function () {
+		$(this).toggleClass('show');
+		$(".footer__column .footer__link").stop().slideUp();
+		$(".footer__column.show .footer__link").stop().slideDown();
+	})
+
+
+	$('.footer__column').click(function () {
+		$('.footer__column').$(this).toggleClass('show');
+	})
+
+
+
+	// ------------------------------------------------------
+
+
+	new Splide('.splide').mount();
+
+
+
+	// не работает
+	$(window).resize(function () {
+		if (window.innerWidth > 968) {
+
+			if ($('.fourth_indicator').hasClass('itcss__indicator_active')) {
+				console.log("Not there in array");
+				$('.fourth_indicator').removeClass('itcss__indicator_active');
+				$('.itcss__indicator_first').addClass('itcss__indicator_active');
+			}
+			else
+
+				if ($('#fifth_indicator').hasClass('itcss__indicator_active')) {
+					console.log("Not there in array");
+					$('#fifth_indicator').removeClass('itcss__indicator_active');
+					$('.itcss__indicator_first').addClass('itcss__indicator_active');
+				}
+		}
+
+	});
+
+
+
+	// $('.itc-slider__indicator').click(function () {
+	// 	$('.slider__indicator').addClass('itcss__indicator_active');
+	// 	this.click
+	// })
+
+
+
+
+
+
+
 
 
 
